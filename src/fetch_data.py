224076234b11f8c4d26fe7eee4a81470b6fe3149
sierrauniformecho <sierra.uniform.echo.lima@gmail.com>
2025-11-19 17:33:55 +0100

    add py files, requirements and notebook for jupyter

---
+++ src/fetch_data.py
@@ -0,0 +1,19 @@
+import requests
+
+## Konstanten
+STOCK_NAME = ""
+COMPANY_NAME = ""
+
+STOCK_ENDPOINT = ""
+NEWS_ENDPOINT = ""
+
+STOCK_API_KEY = ""
+NEWS_API_KEY = ""
+
+## Code
+# Get data historical data
+
+# decide with data we use
+
+# create a list of the receiced data
+
